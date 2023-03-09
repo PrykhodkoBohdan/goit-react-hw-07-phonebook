d4bfdc02281ba6d33c4542f82aaf573887abded3
pickfix <Prikhodko.B.T@gmail.com>
2023-03-09 23:57:28 +0300

    incorect comonent

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,9 +9,12 @@ import { setFilter } from '../redux/filter/filter-slice';
 
 
 import Form from './Form/Form';
-import Section from './Section/Sextion';
+import Section from './Section/Seсtion';
 import Contacts from './Contacts/Contacts';
 import Filter from './Filter/Filter';
+
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -19,42 +22,62 @@ const App = () => {
   const filteredContacts = useSelector(getFilterContacts);
   const filter = useSelector(getFilter);
 
+if(0<1){
+  toast.success('Contact added successfully');
+}
 
-
-  useEffect(() => {
+useEffect(() => {
+  try {
     dispatch(fetchContacts());
+  } catch (error) {
+    console.log(error.message);
+  }
+}, [dispatch]);
 
-  }, [dispatch]);
+const handleFormSubmit = ({ name, number }) => {
+  const isContactExists = contacts.some((contact) => contact.name === name);
 
-  const handleFormSubmit = ({ name, number }) => {
-    const isContactExists = contacts.some(
-      (contact) => contact.name === name
-    );
+  if (isContactExists) {
+    console.log('Contact already exists');
+    return;
+  }
 
-    if (isContactExists) {
-      console.log('Contact already exists');
-      return;
-    }
-
+  try {
     dispatch(fetchAddContact({ name, number }));
-  };
+    toast.success('Contact added successfully');
+  } catch (error) {
+    console.log(error.message);
+    toast.error('Error adding contact');
+  }
+};
 
-  const handleDeleteContact = (id) => {
-    dispatch(fetchDeleteContact (id));
-  };
+const handleDeleteContact = (id) => {
+  try {
+    dispatch(fetchDeleteContact(id));
+    toast.success('Contact deleted successfully');
+  } catch (error) {
+    console.log(error.message);
+    toast.error('Error deleting contact');
+  }
+};
 
   return (
     <>
       <Section title="Phonebook">
         <Form onSubmit={handleFormSubmit} />
       </Section>
-
       <Section title="Contacts">
-        <Filter value={filter} onChange={({ target }) => dispatch(setFilter(target.value))} />
-        <Contacts
-          contacts={filteredContacts.items}
-          onDeleteBtnClick={handleDeleteContact}
-        />
+    
+        <Filter value={filter}  onChange={({ target }) => dispatch(setFilter(target.value))} />
+       
+        {contacts.length ? (
+  <Contacts
+    contacts={filteredContacts}
+    onDeleteBtnClick={handleDeleteContact}
+  />
+) : (
+  <p>No contacts yet</p>
+)}   
       </Section>
     </>
   );
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import css from './Contacts.module.css';
+import PropTypes from "prop-types"
 
-
-const Contacts = ({ contacts, onDeleteBtnClick }) => (
+const Contacts = ({ contacts = [], onDeleteBtnClick }) => (
   <div>
     <ol className={css.number_item}>
       {contacts.map(({name, number, id}) => (
@@ -20,6 +20,17 @@ const Contacts = ({ contacts, onDeleteBtnClick }) => (
     </ol>
   </div>
 );
+
+Contacts.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  onDeleteBtnClick: PropTypes.func.isRequired,
+}
 
 export default Contacts;
 
